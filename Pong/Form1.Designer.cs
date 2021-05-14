ff515f1b71c7d704898976a24a16ccb3ed247662
Chamosiala <alexgroza24@gmail.com>
2021-05-14 15:46:52 +0300

    Add project files.

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Pong
+{
+    partial class Pong
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.paddle1 = new System.Windows.Forms.PictureBox();
+            this.paddle2 = new System.Windows.Forms.PictureBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2Score = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.topBorder = new System.Windows.Forms.PictureBox();
+            this.bottomBorder = new System.Windows.Forms.PictureBox();
+            this.resetLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBorder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomBorder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paddle1
+            // 
+            this.paddle1.BackColor = System.Drawing.Color.LightGray;
+            this.paddle1.Location = new System.Drawing.Point(12, 247);
+            this.paddle1.Name = "paddle1";
+            this.paddle1.Size = new System.Drawing.Size(10, 50);
+            this.paddle1.TabIndex = 0;
+            this.paddle1.TabStop = false;
+            this.paddle1.Tag = "object";
+            // 
+            // paddle2
+            // 
+            this.paddle2.BackColor = System.Drawing.Color.LightGray;
+            this.paddle2.Location = new System.Drawing.Point(778, 247);
+            this.paddle2.Name = "paddle2";
+            this.paddle2.Size = new System.Drawing.Size(10, 50);
+            this.paddle2.TabIndex = 1;
+            this.paddle2.TabStop = false;
+            this.paddle2.Tag = "object";
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 30;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimerEvent);
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player1Score.ForeColor = System.Drawing.Color.White;
+            this.player1Score.Location = new System.Drawing.Point(156, 9);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(46, 54);
+            this.player1Score.TabIndex = 2;
+            this.player1Score.Text = "0";
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player2Score.ForeColor = System.Drawing.Color.White;
+            this.player2Score.Location = new System.Drawing.Point(625, 9);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(46, 54);
+            this.player2Score.TabIndex = 3;
+            this.player2Score.Text = "0";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.LightGray;
+            this.ball.Location = new System.Drawing.Point(404, 266);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(10, 10);
+            this.ball.TabIndex = 4;
+            this.ball.TabStop = false;
+            this.ball.Tag = "";
+            // 
+            // topBorder
+            // 
+            this.topBorder.BackColor = System.Drawing.Color.DimGray;
+            this.topBorder.Location = new System.Drawing.Point(0, 66);
+            this.topBorder.Name = "topBorder";
+            this.topBorder.Size = new System.Drawing.Size(803, 5);
+            this.topBorder.TabIndex = 5;
+            this.topBorder.TabStop = false;
+            this.topBorder.Tag = "border";
+            // 
+            // bottomBorder
+            // 
+            this.bottomBorder.BackColor = System.Drawing.Color.DimGray;
+            this.bottomBorder.Location = new System.Drawing.Point(0, 518);
+            this.bottomBorder.Name = "bottomBorder";
+            this.bottomBorder.Size = new System.Drawing.Size(803, 5);
+            this.bottomBorder.TabIndex = 6;
+            this.bottomBorder.TabStop = false;
+            this.bottomBorder.Tag = "border";
+            // 
+            // resetLabel
+            // 
+            this.resetLabel.AutoSize = true;
+            this.resetLabel.ForeColor = System.Drawing.Color.White;
+            this.resetLabel.Location = new System.Drawing.Point(355, 26);
+            this.resetLabel.Name = "resetLabel";
+            this.resetLabel.Size = new System.Drawing.Size(96, 15);
+            this.resetLabel.TabIndex = 7;
+            this.resetLabel.Text = "Press Esc to reset";
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 535);
+            this.Controls.Add(this.resetLabel);
+            this.Controls.Add(this.bottomBorder);
+            this.Controls.Add(this.topBorder);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player2Score);
+            this.Controls.Add(this.player1Score);
+            this.Controls.Add(this.paddle2);
+            this.Controls.Add(this.paddle1);
+            this.Name = "Pong";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.paddle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBorder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bottomBorder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paddle1;
+        private System.Windows.Forms.PictureBox paddle2;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Label player2Score;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox topBorder;
+        private System.Windows.Forms.PictureBox bottomBorder;
+        private System.Windows.Forms.Label resetLabel;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Pong
+{
+    public partial class Pong : Form
+    {
+        bool moveUp, moveDown;
+        int speed = 10;
+        int[] direction = new int[] { -1, new Random().Next(2) };
+        
+        public Pong()
+        {
+            InitializeComponent();
+        }
+
+        private void mainTimerEvent(object sender, EventArgs e)
+        {
+            ball.Left += speed * direction[0];
+            ball.Top += speed * direction[1];
+
+            if (direction[0] == 1)
+            {
+                if (direction[1] == -1 && paddle2.Top > 75)
+                    paddle2.Top -= speed - 2;
+                if (direction[1] == 1 && paddle2.Top < 465)
+                    paddle2.Top += speed - 2;
+            }
+
+            if (moveUp == true && paddle1.Top > 75)
+                paddle1.Top -= speed;
+            if (moveDown == true && paddle1.Top < 465)
+                paddle1.Top += speed;
+
+            if (ball.Left <= 12)                                                        //resets ball and updates score when missed
+            {
+                ball.Left = 397;
+                ball.Top = 265;
+                player2Score.Text = (Int32.Parse(player2Score.Text) + 1).ToString();
+                
+            }
+            else if (ball.Left >= 792)
+            {
+                ball.Left = 397;
+                ball.Top = 265;
+                direction[0] = -1;
+                player1Score.Text = (Int32.Parse(player1Score.Text) + 1).ToString();
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "object")                       //checks if the ball hit a paddle
+                {
+                    if (ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        if (moveUp == true)
+                            direction[1] = -1;
+                        else if (moveDown == true)
+                            direction[1] = 1;
+
+                        direction[0] *= -1;
+                    }
+                }
+                else if ((string)x.Tag == "border")                                     //checks if the ball hit a border
+                    if (ball.Bounds.IntersectsWith(x.Bounds))    
+                        direction[1] *= -1;
+            }
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    moveUp = true;
+                    break;
+                case Keys.Down:
+                    moveDown = true;
+                    break;
+                case Keys.Escape:
+                    speed = 10;
+                    direction = new int[] { -1, new Random().Next(2) };
+                    player1Score.Text = "0";
+                    player2Score.Text = "0";
+                    ball.Left = 397;
+                    ball.Top = 265;
+                    direction[0] = -1;
+                    paddle1.Left = 12; paddle2.Left = 778;
+                    paddle1.Top = 247; paddle2.Top = 247;
+                    break;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    moveUp = false;
+                    break;
+                case Keys.Down:
+                    moveDown = false;
+                    break;
+            }
+        }
+    }
+}
